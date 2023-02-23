746740dc8e34ef33100749a116f857d91295535c
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2023-02-23 14:48:05 -0800

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Fragment } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <div>
+        {/* Location Information */}
+        {/* Weather Information */}
+      </div>
+      <div>
+        {/* Feels like vs actual (line chart) */}
+        {/* Precipitation (bar chart) */}
+        {/* Visibility & air quality (scatter plot) */}
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
